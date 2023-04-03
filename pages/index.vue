af262a5e31ea06afc7ac79c8a8f11155f9788977
BliahaMuha <hramychev123@gmail.com>
2023-04-03 18:24:37 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section id="main" class="main-alt">
+    <div v-for="post in posts" :key="post.slug" class="blog">
+      <div class="blog-title">{{ post.h1 }}</div>
+      <div class="blog-introtext">{{ post.description }}</div>
+      <div class="blog-tag">{{ post.tags }}</div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  async asyncData(ctx) {
+    const { data } = await axios.get(`http://127.0.0.1:8000/api/posts/`);
+    return {
+      posts: data.results,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
---
+++ pages/sigout.vue
@@ -0,0 +1,13 @@
+<template>
+ <h1>Привет, это страница выход UDhuedueduud</h1>
+</template>
+
+<script>
+export default {
+  name: "sigout"
+}
+</script>
+
+<style scoped>
+
+</style>
